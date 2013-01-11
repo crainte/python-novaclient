b611d33227b37db1cd450bf4dcd6a591b51282e8
Vishvananda Ishaya <vishvananda@gmail.com>
2013-01-11 14:30:41 -0800

    Implement get password for novaclient
    This will download and decrypt a base64 encoded encrypted password
    from the os-server-password extension. It depends on the user
    having openssl installed, but if there is an error of any kind it
    will print out the encoded and encrypted password instead.
    
    It also implements clear_password which will delete the password
    so it can no longer be retrieved.
    
    Change-Id: I2c4e6c3f03b70dc98d6d339381648a6058f46e21
    
    Conflicts:
    
    	tests/v1_1/test_shell.py

--- tests/v1_1/test_shell.py
+++ tests/v1_1/test_shell.py
@@ -798,3 +798,32 @@ class ShellTest(utils.TestCase):
 
         self.run_command('absolute-limits --reserved')
         self.assert_called('GET', '/limits?reserved=1')
+
+    def test_evacuate(self):
+        self.run_command('evacuate sample-server new_host')
+        self.assert_called('POST', '/servers/1234/action',
+                           {'evacuate': {'host': 'new_host',
+                                         'onSharedStorage': False}})
+        self.run_command('evacuate sample-server new_host '
+                            '--password NewAdminPass')
+        self.assert_called('POST', '/servers/1234/action',
+                           {'evacuate': {'host': 'new_host',
+                                         'onSharedStorage': False,
+                                         'adminPass': 'NewAdminPass'}})
+        self.run_command('evacuate sample-server new_host')
+        self.assert_called('POST', '/servers/1234/action',
+                           {'evacuate': {'host': 'new_host',
+                                         'onSharedStorage': False}})
+        self.run_command('evacuate sample-server new_host '
+                            '--on-shared-storage')
+        self.assert_called('POST', '/servers/1234/action',
+                           {'evacuate': {'host': 'new_host',
+                                         'onSharedStorage': True}})
+
+    def test_get_password(self):
+        self.run_command('get-password sample-server /foo/id_rsa')
+        self.assert_called('GET', '/servers/1234/os-server-password')
+
+    def test_clear_password(self):
+        self.run_command('clear-password sample-server')
+        self.assert_called('DELETE', '/servers/1234/os-server-password')
